07244b6d96491751662aca551b2fd73792a65705
matveyplevako <matveyplevako@rambler.ru>
2021-02-01 16:33:58 +0300

    Initial Commit

---
+++ InnoSystemMonitor/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  InnoSystemMonitor
+//
+//  Created by Matvey Plevako on 01.02.2021.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
