30bab198b6424f456308147a4bdbf9817232ed6d
pfh-hbq <pfh.hbq@gmail.com>
2021-02-07 16:22:49 +0300

    refactor2

--- InnoSystemMonitor/Stats/Helpres.swift
+++ InnoSystemMonitor/Stats/Helpres.swift
@@ -1,83 +1,83 @@
 import Foundation
 
 public func fetchIOService(_ name: String) -> [NSDictionary]? {
-    var iterator: io_iterator_t = io_iterator_t()
-    var obj: io_registry_entry_t = 1
-    var list: [NSDictionary] = []
-    
-    let result = IOServiceGetMatchingServices(kIOMasterPortDefault, IOServiceMatching(name), &iterator)
-    if result == kIOReturnSuccess {
-        while obj != 0 {
-            obj = IOIteratorNext(iterator)
-            if let props = getIOProperties(obj) {
-                list.append(props)
-            }
-            IOObjectRelease(obj)
-        }
-        IOObjectRelease(iterator)
-    }
-    
-    return list.isEmpty ? nil : list
+	var iterator: io_iterator_t = io_iterator_t()
+	var obj: io_registry_entry_t = 1
+	var list: [NSDictionary] = []
+	
+	let result = IOServiceGetMatchingServices(kIOMasterPortDefault, IOServiceMatching(name), &iterator)
+	if result == kIOReturnSuccess {
+		while obj != 0 {
+			obj = IOIteratorNext(iterator)
+			if let props = getIOProperties(obj) {
+				list.append(props)
+			}
+			IOObjectRelease(obj)
+		}
+		IOObjectRelease(iterator)
+	}
+	
+	return list.isEmpty ? nil : list
 }
 
 public func getIOProperties(_ entry: io_registry_entry_t) -> NSDictionary? {
-    var properties: Unmanaged<CFMutableDictionary>? = nil
-    
-    if IORegistryEntryCreateCFProperties(entry, &properties, kCFAllocatorDefault, 0) != kIOReturnSuccess {
-        return nil
-    }
-    
-    defer {
-        properties?.release()
-    }
-    
-    return properties?.takeUnretainedValue()
+	var properties: Unmanaged<CFMutableDictionary>? = nil
+	
+	if IORegistryEntryCreateCFProperties(entry, &properties, kCFAllocatorDefault, 0) != kIOReturnSuccess {
+		return nil
+	}
+	
+	defer {
+		properties?.release()
+	}
+	
+	return properties?.takeUnretainedValue()
 }
 
 
 public enum DataSizeBase: String {
-    case bit = "bit"
-    case byte = "byte"
+	case bit = "bit"
+	case byte = "byte"
 }
 
 
 public struct Units {
-    public let bytes: Int64
-    
-    public init(bytes: Int64) {
-        self.bytes = bytes
-    }
-    
-    public var kilobytes: Double {
-        return Double(bytes) / 1_024
-    }
-    public var megabytes: Double {
-        return kilobytes / 1_024
-    }
-    public var gigabytes: Double {
-        return megabytes / 1_024
-    }
-    public var terabytes: Double {
-        return gigabytes / 1_024
-    }
-    
-    public func getReadableTuple(base: DataSizeBase = .byte) -> (String, String) {
-        let stringBase = base == .byte ? "B" : "b"
-        let multiplier: Double = base == .byte ? 1 : 8
-        
-        switch bytes {
-        case 0..<1_024:
-            return ("0", "K\(stringBase)/s")
-        case 1_024..<(1_024 * 1_024):
-            return (String(format: "%.0f", kilobytes*multiplier), "K\(stringBase)/s")
-        case 1_024..<(1_024 * 1_024 * 100):
-            return (String(format: "%.1f", megabytes*multiplier), "M\(stringBase)/s")
-        case (1_024 * 1_024 * 100)..<(1_024 * 1_024 * 1_024):
-            return (String(format: "%.0f", megabytes*multiplier), "M\(stringBase)/s")
-        case (1_024 * 1_024 * 1_024)...Int64.max:
-            return (String(format: "%.1f", gigabytes*multiplier), "G\(stringBase)/s")
-        default:
-            return (String(format: "%.0f", kilobytes*multiplier), "K\(stringBase)B/s")
-        }
-    }
+	public let bytes: Int64
+	
+	public init(bytes: Int64) {
+		self.bytes = bytes
+	}
+	
+	public var kilobytes: Double {
+		return Double(bytes) / 1_024
+	}
+	public var megabytes: Double {
+		return kilobytes / 1_024
+	}
+	public var gigabytes: Double {
+		return megabytes / 1_024
+	}
+	public var terabytes: Double {
+		return gigabytes / 1_024
+	}
+	
+	public func getReadableTuple(base: DataSizeBase = .byte) -> (String, String) {
+		let stringBase = base == .byte ? "B" : "b"
+		let multiplier: Double = base == .byte ? 1 : 8
+		
+		switch bytes {
+		case 0..<1_024:
+			return ("0", "K\(stringBase)/s")
+		case 1_024..<(1_024 * 1_024):
+			return (String(format: "%.0f", kilobytes*multiplier), "K\(stringBase)/s")
+		case 1_024..<(1_024 * 1_024 * 100):
+			return (String(format: "%.1f", megabytes*multiplier), "M\(stringBase)/s")
+		case (1_024 * 1_024 * 100)..<(1_024 * 1_024 * 1_024):
+			return (String(format: "%.0f", megabytes*multiplier), "M\(stringBase)/s")
+		case (1_024 * 1_024 * 1_024)...Int64.max:
+			return (String(format: "%.1f", gigabytes*multiplier), "G\(stringBase)/s")
+		default:
+			return (String(format: "%.0f", kilobytes*multiplier), "K\(stringBase)B/s")
+		}
+	}
 }
--- InnoSystemMonitor/Stats/RAMStats.swift
+++ InnoSystemMonitor/Stats/RAMStats.swift
@@ -2,109 +2,109 @@ import Cocoa
 
 
 public struct RAM_Usage: value_t {
-    var total: Double
-    var used: Double
-    var free: Double
-    
-    var active: Double
-    var inactive: Double
-    var wired: Double
-    var compressed: Double
-    
-    var app: Double
-    var cache: Double
-    var pressure: Double
-    
-    var pressureLevel: Int
-    var swap: Swap
-    
-    public var widget_value: Double {
-        get {
-            return self.usage
-        }
-    }
-    
-    public var usage: Double {
-        get {
-            return Double((self.total - self.free) / self.total)
-        }
-    }
+	var total: Double
+	var used: Double
+	var free: Double
+	
+	var active: Double
+	var inactive: Double
+	var wired: Double
+	var compressed: Double
+	
+	var app: Double
+	var cache: Double
+	var pressure: Double
+	
+	var pressureLevel: Int
+	var swap: Swap
+	
+	public var widget_value: Double {
+		get {
+			return self.usage
+		}
+	}
+	
+	public var usage: Double {
+		get {
+			return Double((self.total - self.free) / self.total)
+		}
+	}
 }
 
 public struct Swap {
-    var total: Double
-    var used: Double
-    var free: Double
+	var total: Double
+	var used: Double
+	var free: Double
 }
 
 public class RAMStats {
-    
-    public func read(callback: @escaping ([TopProcess]) -> ()) {
-        let numberOfProcesses = 10
-        
-        if numberOfProcesses == 0 {
-            return
-        }
-        
-        let task = Process()
-        task.launchPath = "/usr/bin/top"
-        task.arguments = ["-l", "1", "-o", "mem", "-n", "\(numberOfProcesses)", "-stats", "pid,command,mem"]
-        
-        let outputPipe = Pipe()
-        let errorPipe = Pipe()
-        
-        task.standardOutput = outputPipe
-        task.standardError = errorPipe
-        
-        do {
-            try task.run()
-        } catch let error {
-            print("Error: \(error.localizedDescription)")
-            return
-        }
-        
-        let outputData = outputPipe.fileHandleForReading.readDataToEndOfFile()
-        let errorData = errorPipe.fileHandleForReading.readDataToEndOfFile()
-        let output = String(decoding: outputData, as: UTF8.self)
-        _ = String(decoding: errorData, as: UTF8.self)
-        
-        if output.isEmpty {
-            return
-        }
-        
-        var processes: [TopProcess] = []
-        output.enumerateLines { (line, _) -> () in
-            if line.matches("^\\d+ +.* +\\d+[A-Z]*\\+?\\-? *$") {
-                var str = line.trimmingCharacters(in: .whitespaces)
-                let pidString = str.findAndCrop(pattern: "^\\d+")
-                let usageString = str.suffix(5)
-                var command = str.replacingOccurrences(of: pidString, with: "")
-                command = command.replacingOccurrences(of: usageString, with: "")
-                
-                if let regex = try? NSRegularExpression(pattern: " (\\+|\\-)*$", options: .caseInsensitive) {
-                    command = regex.stringByReplacingMatches(in: command, options: [], range: NSRange(location: 0, length:  command.count), withTemplate: "")
-                }
-                
-                let pid = Int(pidString.filter("01234567890.".contains)) ?? 0
-                var usage = Double(usageString.filter("01234567890.".contains)) ?? 0
-                if usageString.contains("G") {
-                    usage *= 1024 // apply gigabyte multiplier
-                } else if usageString.contains("K") {
-                    usage /= 1024 // apply kilobyte divider
-                }
-                
-                var name: String? = nil
-                var icon: NSImage? = nil
-                if let app = NSRunningApplication(processIdentifier: pid_t(pid) ) {
-                    name = app.localizedName ?? nil
-                    icon = app.icon
-                }
-                
-                let process = TopProcess(pid: pid, command: command, name: name, usage: usage * Double(1024 * 1024), icon: icon)
-                processes.append(process)
-            }
-        }
-        
-        callback(processes)
-    }
+	
+	public func read(callback: @escaping ([TopProcess]) -> ()) {
+		let numberOfProcesses = 10
+		
+		if numberOfProcesses == 0 {
+			return
+		}
+		
+		let task = Process()
+		task.launchPath = "/usr/bin/top"
+		task.arguments = ["-l", "1", "-o", "mem", "-n", "\(numberOfProcesses)", "-stats", "pid,command,mem"]
+		
+		let outputPipe = Pipe()
+		let errorPipe = Pipe()
+		
+		task.standardOutput = outputPipe
+		task.standardError = errorPipe
+		
+		do {
+			try task.run()
+		} catch let error {
+			print("Error: \(error.localizedDescription)")
+			return
+		}
+		
+		let outputData = outputPipe.fileHandleForReading.readDataToEndOfFile()
+		let errorData = errorPipe.fileHandleForReading.readDataToEndOfFile()
+		let output = String(decoding: outputData, as: UTF8.self)
+		_ = String(decoding: errorData, as: UTF8.self)
+		
+		if output.isEmpty {
+			return
+		}
+		
+		var processes: [TopProcess] = []
+		output.enumerateLines { (line, _) -> () in
+			if line.matches("^\\d+ +.* +\\d+[A-Z]*\\+?\\-? *$") {
+				var str = line.trimmingCharacters(in: .whitespaces)
+				let pidString = str.findAndCrop(pattern: "^\\d+")
+				let usageString = str.suffix(5)
+				var command = str.replacingOccurrences(of: pidString, with: "")
+				command = command.replacingOccurrences(of: usageString, with: "")
+				
+				if let regex = try? NSRegularExpression(pattern: " (\\+|\\-)*$", options: .caseInsensitive) {
+					command = regex.stringByReplacingMatches(in: command, options: [], range: NSRange(location: 0, length:  command.count), withTemplate: "")
+				}
+				
+				let pid = Int(pidString.filter("01234567890.".contains)) ?? 0
+				var usage = Double(usageString.filter("01234567890.".contains)) ?? 0
+				if usageString.contains("G") {
+					usage *= 1024 // apply gigabyte multiplier
+				} else if usageString.contains("K") {
+					usage /= 1024 // apply kilobyte divider
+				}
+				
+				var name: String? = nil
+				var icon: NSImage? = nil
+				if let app = NSRunningApplication(processIdentifier: pid_t(pid) ) {
+					name = app.localizedName ?? nil
+					icon = app.icon
+				}
+				
+				let process = TopProcess(pid: pid, command: command, name: name, usage: usage * Double(1024 * 1024), icon: icon)
+				processes.append(process)
+			}
+		}
+		
+		callback(processes)
+	}
 }
