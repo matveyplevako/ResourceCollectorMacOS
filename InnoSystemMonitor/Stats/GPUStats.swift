b0af6a72fe836bd8c3cbe4d4cfa5e7fbbe7940fb
pfh-hbq <pfh.hbq@gmail.com>
2021-02-08 21:47:44 +0300

    more deletion more refactor

--- InnoSystemMonitor/Stats/GPUStats.swift
+++ InnoSystemMonitor/Stats/GPUStats.swift
@@ -2,54 +2,23 @@ import Foundation
 import Cocoa
 
 class GPUStats {
-	
-	init() {
-	}
-	
 	public struct GPUs {
 		public var list: [GPU_Info] = []
-		
-		internal func active() -> [GPU_Info] {
-			return self.list.filter{ $0.state && $0.utilization != nil }.sorted{ $0.utilization ?? 0 > $1.utilization ?? 0 }
-		}
 	}
-	
-	public typealias GPU_type = String
 	
 	public struct GPU_Info {
-		
 		public let IOClass: String
-		public var vendor: String? = nil
-		public let model: String
-		
-		public var state: Bool = true
-		
+		public let GPUmodel: String
+
 		public var utilization: Double? = nil
 		
-		init(IOClass: String, vendor: String? = nil, model: String) {
+		init(IOClass: String, model: String) {
 			self.IOClass = IOClass
-			self.vendor = vendor
-			self.model = model
+			self.GPUmodel = model
 		}
 	}
 	
-	public enum GPU_types: GPU_type {
-		case unknown = ""
-		case integrated = "i"
-		case external = "e"
-		case discrete = "d"
-	}
-	
-	public struct device {
-		public let vendor: String?
-		public let model: String
-		public let pci: String
-		public var used: Bool
-	}
-	
-	internal var smc: UnsafePointer<SMCService>? = nil
 	private var gpus: GPUs = GPUs()
-	private var devices: [device] = []
 	
 	public func read(callback: @escaping (GPUs) -> Void) {
 		guard let accelerators = fetchIOService(kIOAcceleratorClassName) else {
@@ -67,64 +36,32 @@ class GPUStats {
 				return
 			}
 			
-			var vendor: String? = nil
-			var model: String = ""
-			let accMatch = (accelerator["IOPCIMatch"] as? String ?? accelerator["IOPCIPrimaryMatch"] as? String ?? "").lowercased()
-			
-			for (i, device) in devices.enumerated() {
-				if accMatch.range(of: device.pci) != nil && !device.used {
-					model = device.model
-					vendor = device.vendor
-					devices[i].used = true
-					break
-				}
-			}
-			
 			let ioClass = IOClass.lowercased()
-			var predictModel = ""
+			var GPUmodel: String = ""
 			
 			let utilization: Int? = stats["Device Utilization %"] as? Int ?? stats["GPU Activity(%)"] as? Int ?? nil
 			
-			if ioClass == "nvaccelerator" || ioClass.contains("nvidia") { // nvidia
-				predictModel = "Nvidia Graphics"
-			} else if ioClass.contains("amd") { // amd
-				predictModel = "AMD Graphics"
-			} else if ioClass.contains("intel") { // intel
-				predictModel = "Intel Graphics"
-			} else if ioClass.contains("agx") { // apple
-				predictModel = stats["model"] as? String ?? "Apple Graphics"
+			if ioClass == "nvaccelerator" || ioClass.contains("nvidia") {
+				GPUmodel = "Nvidia Graphics"
+			} else if ioClass.contains("amd") {
+				GPUmodel = "AMD Graphics"
 			} else {
-				predictModel = "Unknown"
+				GPUmodel = "Intel Graphics"
 			}
 			
-			if model == "" {
-				model = predictModel
-			}
-			if let v = vendor {
-				model = model.removedRegexMatches(pattern: v, replaceWith: "").trimmingCharacters(in: .whitespacesAndNewlines)
-			}
-			
-			if self.gpus.list.first(where: { $0.model == model }) == nil {
+			if self.gpus.list.first(where: { $0.GPUmodel == GPUmodel }) == nil {
 				self.gpus.list.append(GPU_Info(
 					IOClass: IOClass,
-					vendor: vendor,
-					model: model
+					model: GPUmodel
 				))
 			}
-			guard let idx = self.gpus.list.firstIndex(where: { $0.model == model }) else {
+			guard let idx = self.gpus.list.firstIndex(where: { $0.GPUmodel == GPUmodel }) else {
 				return
 			}
-			
-			if let agcInfo = accelerator["AGCInfo"] as? [String:Int], let state = agcInfo["poweredOffByAGC"] {
-				self.gpus.list[idx].state = state == 0
-			}
-			
 			if let value = utilization {
 				self.gpus.list[idx].utilization = Double(value)/100
 			}
 		}
-		
-		self.gpus.list.sort{ !$0.state && $1.state }
 		callback(self.gpus)
 	}
 }
