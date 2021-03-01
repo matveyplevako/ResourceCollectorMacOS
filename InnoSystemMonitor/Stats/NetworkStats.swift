679bc1d1e940a789397e9baf1e0a76679235f895
pfh-hbq <pfh.hbq@gmail.com>
2021-03-01 21:58:41 +0300

    moved the trash out, almost half))

--- InnoSystemMonitor/Stats/NetworkStats.swift
+++ InnoSystemMonitor/Stats/NetworkStats.swift
@@ -6,29 +6,22 @@ import CoreWLAN
 public struct Network_Process {
     var time: Date = Date()
     var name: String = ""
-    var pid: String = ""
     var download: Int = 0
     var upload: Int = 0
-    var icon: NSImage? = nil
 }
 
 public typealias Bandwidth = (upload: Int64, download: Int64)
+
 public struct Network_Usage: value_t {
     var bandwidth: Bandwidth = (0, 0)
     var total: Bandwidth = (0, 0)
     
     var laddr: String? = nil // local ip
-    var raddr: String? = nil // remote ip
-    
-    var ssid: String? = nil
     
     mutating func reset() {
         self.bandwidth = (0, 0)
         
         self.laddr = nil
-        self.raddr = nil
-	
-        self.ssid = nil
     }
     
     public var widget_value: Double = 0
@@ -38,24 +31,6 @@ public class NetworkStats: ReaderProtocol {
     public var store: UnsafePointer<Store>? = nil
     
     private var usage: Network_Usage = Network_Usage()
-    
-    private var primaryInterface: String {
-        get {
-            if let global = SCDynamicStoreCopyValue(nil, "State:/Network/Global/IPv4" as CFString), let name = global["PrimaryInterface"] as? String {
-                return name
-            }
-            return ""
-        }
-    }
-    
-    private var interfaceID: String {
-        get {
-            return self.store?.pointee.string(key: "Network_interface", defaultValue: self.primaryInterface) ?? self.primaryInterface
-        }
-        set {
-            self.store?.pointee.set(key: "Network_interface", value: newValue)
-        }
-    }
     
     private var reader: String {
         get {
@@ -97,10 +72,7 @@ public class NetworkStats: ReaderProtocol {
         var pointer = interfaceAddresses
         while pointer != nil {
             defer { pointer = pointer?.pointee.ifa_next }
-            
-            if String(cString: pointer!.pointee.ifa_name) != self.interfaceID {
-                continue
-            }
+     
             
             if let ip = getLocalIP(pointer!), self.usage.laddr != ip {
                 self.usage.laddr = ip
@@ -122,10 +94,8 @@ public class NetworkStats: ReaderProtocol {
         task.arguments = ["-P", "-L", "1", "-k", "time,interface,state,rx_dupe,rx_ooo,re-tx,rtt_avg,rcvsize,tx_win,tc_class,tc_mgt,cc_algo,P,C,R,W,arch"]
         
         let outputPipe = Pipe()
-        let errorPipe = Pipe()
         
         task.standardOutput = outputPipe
-        task.standardError = errorPipe
         
         do {
             try task.run()
@@ -135,9 +105,8 @@ public class NetworkStats: ReaderProtocol {
         }
         
         let outputData = outputPipe.fileHandleForReading.readDataToEndOfFile()
-        let errorData = errorPipe.fileHandleForReading.readDataToEndOfFile()
         let output = String(decoding: outputData, as: UTF8.self)
-        _ = String(decoding: errorData, as: UTF8.self)
+        
         
         if output.isEmpty {
             return (0, 0)
@@ -168,14 +137,6 @@ public class NetworkStats: ReaderProtocol {
         return (totalUpload, totalDownload)
     }
     
-    public func getDetails() {
-        self.usage.reset()
-        
-        DispatchQueue.global(qos: .background).async {
-            self.getPublicIP()
-        }
-    }
-    
     private func getLocalIP(_ pointer: UnsafeMutablePointer<ifaddrs>) -> String? {
         var addr = pointer.pointee.ifa_addr.pointee
         
@@ -189,18 +150,6 @@ public class NetworkStats: ReaderProtocol {
         return String(cString: ip)
     }
     
-    private func getPublicIP() {
-        let url = URL(string: "https://api.ipify.org")
-        
-        do {
-            if let url = url {
-                self.usage.raddr = try String(contentsOf: url)
-            }
-        } catch let error {
-//			yoho catched error!)
-        }
-    }
-    
     private func getBytesInfo(_ pointer: UnsafeMutablePointer<ifaddrs>) -> (upload: Int64, download: Int64)? {
         let addr = pointer.pointee.ifa_addr.pointee
         
@@ -210,134 +159,5 @@ public class NetworkStats: ReaderProtocol {
         
         let data: UnsafeMutablePointer<if_data>? = unsafeBitCast(pointer.pointee.ifa_data, to: UnsafeMutablePointer<if_data>.self)
         return (upload: Int64(data?.pointee.ifi_obytes ?? 0), download: Int64(data?.pointee.ifi_ibytes ?? 0))
-    }
-}
-
-public class ProcessReader {
-    private let store: UnsafePointer<Store>
-    private let title: String
-    private var previous: [Network_Process] = []
-    
-    private var numberOfProcesses: Int {
-        get {
-            return self.store.pointee.int(key: "\(self.title)_processes", defaultValue: 8)
-        }
-    }
-    
-    init(_ title: String, store: UnsafePointer<Store>) {
-        self.title = title
-        self.store = store
-    }
-    
-    public func read() {
-        if self.numberOfProcesses == 0 {
-            return
-        }
-        
-        let task = Process()
-        task.launchPath = "/usr/bin/nettop"
-        task.arguments = ["-P", "-L", "1", "-k", "time,interface,state,rx_dupe,rx_ooo,re-tx,rtt_avg,rcvsize,tx_win,tc_class,tc_mgt,cc_algo,P,C,R,W,arch"]
-        
-        let outputPipe = Pipe()
-        let errorPipe = Pipe()
-        
-        task.standardOutput = outputPipe
-        task.standardError = errorPipe
-        
-        do {
-            try task.run()
-        } catch let error {
-            print(error)
-            return
-        }
-        
-        let outputData = outputPipe.fileHandleForReading.readDataToEndOfFile()
-        let errorData = errorPipe.fileHandleForReading.readDataToEndOfFile()
-        let output = String(decoding: outputData, as: UTF8.self)
-        _ = String(decoding: errorData, as: UTF8.self)
-        
-        if output.isEmpty {
-            return
-        }
-        
-        var list: [Network_Process] = []
-        var firstLine = false
-        output.enumerateLines { (line, _) -> () in
-            if !firstLine {
-                firstLine = true
-                return
-            }
-            
-            let parsedLine = line.split(separator: ",")
-            guard parsedLine.count >= 3 else {
-                return
-            }
-            
-            var process = Network_Process()
-            process.time = Date()
-            
-            let nameArray = parsedLine[0].split(separator: ".")
-            if let pid = nameArray.last {
-                process.pid = String(pid)
-            }
-            if let app = NSRunningApplication(processIdentifier: pid_t(process.pid) ?? 0) {
-                process.name = app.localizedName ?? nameArray.dropLast().joined(separator: ".")
-                process.icon = app.icon
-            } else {
-                process.name = nameArray.dropLast().joined(separator: ".")
-            }
-            
-            if let download = Int(parsedLine[1]) {
-                process.download = download
-            }
-            if let upload = Int(parsedLine[2]) {
-                process.upload = upload
-            }
-            
-            list.append(process)
-        }
-        
-        var processes: [Network_Process] = []
-        if self.previous.count == 0 {
-            self.previous = list
-            processes = list
-        } else {
-            self.previous.forEach { (pp: Network_Process) in
-                if let i = list.firstIndex(where: { $0.pid == pp.pid }) {
-                    let p = list[i]
-                    
-                    var download = p.download - pp.download
-                    var upload = p.upload - pp.upload
-                    let time = download == 0 && upload == 0 ? pp.time : Date()
-                    list[i].time = time
-                    
-                    if download < 0 {
-                        download = 0
-                    }
-                    if upload < 0 {
-                        upload = 0
-                    }
-                    
-                    processes.append(Network_Process(time: time, name: p.name, pid: p.pid, download: download, upload:  upload, icon: p.icon))
-                }
-            }
-            self.previous = list
-        }
-        
-        processes.sort {
-            let firstMax = max($0.download, $0.upload)
-            let secondMax = max($1.download, $1.upload)
-            let firstMin = min($0.download, $0.upload)
-            let secondMin = min($1.download, $1.upload)
-            
-            if firstMax == secondMax && firstMin == secondMin { // download and upload values are the same, sort by time
-                return $0.time < $1.time
-            } else if firstMax == secondMax && firstMin != secondMin { // max values are the same, min not. Sort by min values
-                return firstMin < secondMin
-            }
-            return firstMax < secondMax // max values are not the same, sort by max value
-        }
-        
-//        self.callback(processes.suffix(self.numberOfProcesses).reversed())
     }
 }
