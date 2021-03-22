601d2f12f4afffed3167fe8edb94c39b4e186d71
pfh-hbq <pfh.hbq@gmail.com>
2021-03-22 13:13:43 +0300

    refactor

--- InnoSystemMonitorTests/CPUStatsTest.swift
+++ InnoSystemMonitorTests/CPUStatsTest.swift
@@ -1,10 +1,3 @@
-//
-//  CPUStatsTest.swift
-//  InnoSystemMonitorTests
-//
-//  Created by Matvey Plevako on 15.02.2021.
-//
-
 import XCTest
 @testable import InnoSystemMonitor
 
--- InnoSystemMonitorTests/RAMStatsTest.swift
+++ InnoSystemMonitorTests/RAMStatsTest.swift
@@ -1,10 +1,3 @@
-//
-//  RAMStatsTest.swift
-//  InnoSystemMonitorTests
-//
-//  Created by Matvey Plevako on 16.02.2021.
-//
-
 import XCTest
 @testable import InnoSystemMonitor
 
